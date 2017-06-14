6d64ed595d9539bc584753714526273853db0424
Tony Morris <tmorris@tmorris.net>
2017-06-14 13:42:49 +1000

    aipchart

---
+++ aipchart.hs
@@ -0,0 +1,96 @@
+-- http://www.airservicesaustralia.com/aip/current/aipchart/
+
+erch =
+  [
+  , "erch/erch1.pdf"
+  , "erch/erch2.pdf"
+  , "erch/erch3.pdf"
+  , "erch/erch4.pdf"
+  , "erch/erch5.pdf"
+  ]
+
+ercl =
+  [
+  , "ercl/ercl1.pdf"
+  , "ercl/ercl2.pdf"
+  , "ercl/ercl3.pdf"
+  , "ercl/ercl4.pdf"
+  , "ercl/ercl5.pdf"
+  , "ercl/ercl6.pdf"
+  , "ercl/ercl7.pdf"
+  , "ercl/ercl8.pdf"
+  ]
+
+pca =
+  [
+  , "pca/PCA_back.pdf"
+  , "pca/PCA_front.pdf"
+  ]
+
+tac =
+  [
+  , "tac/tac1.pdf"
+  , "tac/tac2.pdf"
+  , "tac/tac3.pdf"
+  , "tac/tac4.pdf"
+  , "tac/tac5.pdf"
+  , "tac/tac6.pdf"
+  , "tac/tac7.pdf"
+  , "tac/tac8.pdf"
+  ]
+
+vnc =
+  [
+  , "vnc/Adelaide_VNC.pdf"
+  , "vnc/Brisbane_VNC.pdf"
+  , "vnc/Bundaberg_VNC.pdf"
+  , "vnc/Cairns_VNC.pdf"
+  , "vnc/Darwin_VNC.pdf"
+  , "vnc/Hobart_VNC.pdf"
+  , "vnc/Launceston_VNC.pdf"
+  , "vnc/Melbourne_VNC.pdf"
+  , "vnc/Newcastle_VNC.pdf"
+  , "vnc/Perth_VNC.pdf"
+  , "vnc/Rockhampton_VNC.pdf"
+  , "vnc/Sydney_VNC.pdf"
+  , "vnc/Tindal_VNC.pdf"
+  , "vnc/Townsville_VNC.pdf"
+  ]
+
+vtc =
+  [
+  , "vtc/Adelaide_VTC.pdf"
+  , "vtc/Albury_VTC.pdf"
+  , "vtc/AliceSprings_Uluru_VTC.pdf"
+  , "vtc/Brisbane_Sunshine_VTC.pdf"
+  , "vtc/Broome_VTC.pdf"
+  , "vtc/Cairns_VTC.pdf"
+  , "vtc/Canberra_VTC.pdf"
+  , "vtc/Coffs_Harbour_VTC.pdf"
+  , "vtc/Darwin_VTC.pdf"
+  , "vtc/Gold_Coast_VTC.pdf"
+  , "vtc/Hobart_VTC.pdf"
+  , "vtc/Karratha_VTC.pdf"
+  , "vtc/Launceston_VTC.pdf"
+  , "vtc/Mackay_VTC.pdf"
+  , "vtc/Melbourne_VTC.pdf"
+  , "vtc/Newcastle_Williamtown_VTC.pdf"
+  , "vtc/Oakey_Bris_VTC.pdf"
+  , "vtc/perth_legend.pdf"
+  , "vtc/Perth_VTC.pdf"
+  , "vtc/Rockhampton_VTC.pdf"
+  , "vtc/Sydney_VTC.pdf"
+  , "vtc/Tamworth_VTC.pdf"
+  , "vtc/Townsville_VTC.pdf"
+  , "vtc/Whitsunday_VTC.pdf"
+  ]
+
+all =
+  [
+    erch
+  , ercl
+  , pca
+  , tac
+  , vnc
+  , vtc
+  ]
